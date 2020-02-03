eecbbd1315ed7c44066059f7419db1c5d0c1670c
keshibat <kensukeshibata@gmail.com>
2020-02-03 16:05:09 +0900

    useState hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>  {
+  const [resource, setResource] = useState('posts');
+    return (
+      <div>
+        <div>
+          <button onClick={() => setResource('posts')}>Posts</button>
+          <button onClick={() => setResource('todos')}>Todos</button>
+        </div>
+        {resource}
+      </div>
+    );
+  }
 
 export default App;
